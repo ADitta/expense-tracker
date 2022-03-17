8967c8e65c5b410d8a42629da2e1724b701ffd03
= <=>
2022-03-17 12:26:52 +0000

    complete basic functionality with styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import "./styles/App.scss";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import Expenses from "./components/Expenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+
+import { GlobalProvider } from "./context/GlobalState";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <Expenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
 }
 
 export default App;
